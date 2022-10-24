4e6ee2e2ea6a647a8c13ab9b997be231b246fa5c
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-10-24 12:21:22 -0600

    adding more testing functionality to login

--- adskfaysufgyasd/src/App.js
+++ adskfaysufgyasd/src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [data, setData] = useState("")
+  const [emailCreate, setEmailCreate] = useState("")
+  const [passwordCreate, setPasswordCreate] = useState("")
 
   console.log(data)
 
@@ -31,17 +33,45 @@ function App() {
         r.json().then(json => console.log(Object.entries(json.errors)))
       }
     })
-    .catch(err => console.log(err))
-    .finally(data => {
-      setData(data)
+  }
+
+  function handleSubmitCreate(e){
+    e.preventDefault();
+    const user = {
+      email: emailCreate,
+      password: passwordCreate,
+    }
+    fetch('/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(user)
+    })
+    .then(r => {
+      if(r.ok){
+        r.json().then(data => {
+          setData(data)
+        })
+      } else {
+        r.json().then(json => console.log(Object.entries(json.errors)))
+      }
     })
   }
 
   return (
     <div>
+      <p>sign in:</p>
       <form onSubmit={handleSubmit}>
         <input value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="email"></input>
         <input  value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="password"></input>
+        <button type="submit">submit</button>
+      </form>
+      <br/>
+      <p>create users:</p>
+      <form onSubmit={handleSubmitCreate}>
+        <input value={emailCreate} onChange={e => setEmailCreate(e.target.value)} type="text" placeholder="email"></input>
+        <input  value={passwordCreate} onChange={e => setPasswordCreate(e.target.value)} type="password" placeholder="password"></input>
         <button type="submit">submit</button>
       </form>
     </div>
