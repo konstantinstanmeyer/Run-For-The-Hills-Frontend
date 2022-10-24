4ecb0a25bd362e3247d97759b0190c56ab1cc43e
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-10-24 13:38:46 -0600

    adding imports for reactrouter

--- adskfaysufgyasd/src/App.js
+++ adskfaysufgyasd/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
