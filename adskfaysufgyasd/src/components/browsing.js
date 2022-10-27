26553daadb0a10f25f7b7f8f5ca64d279a125aa1
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-10-27 16:05:04 -0600

    finishing browsing page

--- adskfaysufgyasd/src/components/browsing.js
+++ adskfaysufgyasd/src/components/browsing.js
@@ -5,23 +5,139 @@ import { Link } from "react-router-dom";
 
 
 export default function Browsing({ allProfiles, current_user_id }) {
+    const [like, setLike] = useState();
+    const [dislike, setDislike] = useState();
+    const [goatAmount, setGoatAmount]  = useState();
+    const goatButton = document.querySelector('#goat')
+    const boatButton = document.querySelector('#boat')
+    const [goat, setGoat] = useState(false);
+    const [boat, setBoat] = useState(false);
+    const [birthaProximity, setBirthaProximity] = useState(0);
+    const [moonshineAbv, setMoonshineAbv] = useState(0);
+    const [buckles, setBuckles] = useState(0);
 
+    function handleGoatClick(){
+        if(goat){
+            goatButton.classList.remove('bg-lime-500')
+            goatButton.classList.add('bg-red-500')
+            setGoat(!goat)
+        } else if (!goat){
+            goatButton.classList.remove('bg-red-500')
+            goatButton.classList.add('bg-lime-500')
+            setGoat(!goat)
+        }
+    }
 
+    function handleBoatClick(){
+        if(boat){
+            boatButton.classList.remove('bg-lime-500')
+            boatButton.classList.add('bg-red-500')
+            setBoat(!boat)
+        } else if (!boat){
+            boatButton.classList.remove('bg-red-500')
+            boatButton.classList.add('bg-lime-500')
+            setBoat(!boat)
+        }
+    }
+
+    function checkProfile(profile){
+        return profile.moonshine_abv_level >= moonshineAbv && profile.rodeo_buckles >= buckles
+    }
+
+    const displayedUsers = allProfiles.filter(checkProfile)
     console.log(allProfiles)
 
+    console.log(displayedUsers)
     return (
         <div className="">
-            <img className="w-screen h-screen object-cover fixed -z-10" src="https://images.unsplash.com/photo-1440428099904-c6d459a7e7b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>     
-            <div id="browsing-sidebar" className="fixed left-0 w-1/4 bg-white/70 backdrop-blur-sm h-full">
-            <p>
-                Tired of searching?? Check out your <Link to="/likesmatches"> likes and matches</Link> instead! 
-            </p>
-            <p>
-                Time to get spicy 🌶️ 🫑 🥵
-            </p>
+            <img className="w-screen h-screen object-cover bg-bottom fixed -z-10" src="https://images.unsplash.com/photo-1440428099904-c6d459a7e7b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>     
+            <div id="browsing-sidebar" className="fixed left-0 w-1/4 h-full bg-white/70 overflow-scroll backdrop-blur-sm shadow-[0_0px_20px_-15px]">
+                <h2 className="text-3xl font-bold text-center pt-[20%] pb-0">
+                    Time to get spicy
+                <br />
+                    🌶️ 🫑 🥵
+                </h2>
+                <div className="mx-auto h-[3px] w-3/4 bg-gray-300 my-5"></div>
+                <h3 className="text-xl text-center m-0 p-0">raise your standards!</h3>
+                <div className="flex flex-col justify-center mb-3">
+                    <h4 className="mx-auto text-lg font-bold py-2">Birtha Proximity</h4>
+                    <div className="flex align-item w-2/3 mx-auto relative">
+                        <p className="font-bold">none</p>
+                        <input
+                            id="range2"
+                            value={birthaProximity}
+                            onChange={(e) => setBirthaProximity(e.target.value)}
+                            type="range"
+                            min="0"
+                            max="100"
+                            step="20"
+                            className="w-2/3 mx-auto appearance-none h-4 rounded-3xl bg-white shadow-md border-2"
+                        />
+                    <p className="font-bold">100</p>
+                    </div>
+                    <h4 className="mx-auto text-lg font-bold py-2">Buckles</h4>
+                    <div className="flex align-item w-2/3 mx-auto relative">
+                        <p className="font-bold">0</p>
+                        <input 
+                            id="range2"
+                            value={buckles}
+                            onChange={(e) => setBuckles(e.target.value)}
+                            type="range"
+                            min="0"
+                            max="100"
+                            step="20"
+                            className="w-2/3 mx-auto appearance-none h-4 rounded-3xl bg-white shadow-md border-2"
+                        />
+                        <p className="font-bold">100</p>
+                    </div>
+                    <h4 className="mx-auto text-lg font-bold py-2">Moonshine Abv Level</h4>
+                    <div className="flex align-item w-2/3 mx-auto relative">
+                        <p className="font-bold">0%</p>
+                        <input 
+                            id="range2"
+                            value={moonshineAbv}
+                            onChange={(e) => setMoonshineAbv(e.target.value)}
+                            type="range"
+                            min="0"
+                            max="100"
+                            step="20"
+                            className="w-2/3 mx-auto appearance-none h-4 rounded-3xl bg-white shadow-md border-2"
+                        />
+                        <p className="font-bold">100%</p>
+                    </div>
+                </div>
+                <button id="goat" onClick={() => handleGoatClick()} className={`text-4xl w-1/2 h-12 ml-[25%] bg-red-500 rounded-3xl my-3`}><img className="mx-auto w-8 h-8" src="https://cdn-icons-png.flaticon.com/512/1998/1998662.png"/></button>
+                <button id="boat" onClick={() => handleBoatClick()} className={`text-4xl w-1/2 h-12 ml-[25%] bg-red-500 rounded-3xl my-3`}><img className="mx-auto w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/853/853507.png"/></button>
+                <p className="text-center w-1/2 mx-auto mt-2">
+                    Tired of searching?? Check out your <Link className="text-blue-700 hover:underline" to="/likesmatches"> likes and matches</Link> instead! 
+                </p>
+            </div>
+            <div className="w-3/4 ml-auto h-fit relative pt-10">
+                <div className="fixed w-3/4 z-20">
+                    <div className="bg-white flex flex-row items-center w-3/4 h-14 mx-auto rounded-xl mb-7 shadow-md relative">
+                        <h3 className="text-lg ml-2 font-bold w-fit px-2 rounded-md hover:underline">Run for the Hills</h3>
+                        <li className="px-2 list-none">
+                            <a className="hover:cursor-pointer hover:underline">Blog</a>
+                        </li>
+                        <li className="px-2 list-none">
+                            <a className="hover:cursor-pointer hover:underline">Contact Us</a>
+                        </li>
+                        <div className="ml-auto flex flex-row items-center">
+                            <ul className="list-none p-0 flex flex-row mr-2">
+                                <li className="px-2">
+                                    <a className="hover:cursor-pointer hover:underline">Matches</a>
+                                </li>
+                            </ul>
+                            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="h-10 w-10 mr-4"/>
+                        </div>
+                    </div>
+                </div>
+                <h1 className="text-center mt-20 text-4xl font-bold text-lime-500">
+                    "alphas don't need to be lone wolves, join the pack"
+                </h1>
             </div>
             <div id="profile-list" className="w-3/4 flex flex-wrap ml-auto">
-                {allProfiles.map((eachProfile) =>
+                {displayedUsers.map((eachProfile) =>
                         <RenderProfileCard
                             key={uuid()}
                             picture={eachProfile.profile_picture}
@@ -41,83 +157,3 @@ export default function Browsing({ allProfiles, current_user_id }) {
         </div>
     )
 }
-        // let fakeProfiles =  [
-    //     {
-    //         id: 2,
-    //         name: "David Blahblahblah",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     },
-    //     {
-    //         id: 4,
-    //         name: "really long name things",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     },
-    //     {
-    //         id: 3,
-    //         name: "really long name things",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     },
-    //     {
-    //         id: 1,
-    //         name: "really long name things",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     },
-    //     {
-    //         id: 7,
-    //         name: "really long name things",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     },
-    //     {
-    //         id: 5,
-    //         name: "really long name things",
-    //         profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
-    //         photo_png: 'nill',
-    //         beard_length: 0,
-    //         moonshine_abv_level: 21.3,
-    //         rodeo_buckles: 0,
-    //         truck_brand: 'chevy',
-    //         mode_of_tobacco: 'chew',
-    //         "pontoon_boat?": false,
-    //         "security_goat?": false,
-    //     }
-    // ]
--- adskfaysufgyasd/src/components/renderprofilecard.js
+++ adskfaysufgyasd/src/components/renderprofilecard.js
@@ -5,6 +5,7 @@ import style from "../car.module.css";
 export default function RenderProfileCard({ picture, beard, moonshine, rodeo_buckles, truck_brand, mode_of_tobacco, pontoon_boat, security_goat, current_user_id, thisCardUserData }){
     const [isClicked, setIsClicked] = useState(false);
     const [car, setCar] = useState(false);
+    const fullName = `${thisCardUserData.first_name} ${thisCardUserData.last_name} `;
 
     //console.log(thisCardUserData)
 
@@ -65,16 +66,19 @@ export default function RenderProfileCard({ picture, beard, moonshine, rodeo_buc
     }
 
     const profileCardFront = (
-        <>
+        <>      
+
+            <div className="py-5">
             <img
-                src={car ? "https://www.chevytrucklegends.com/content/dam/Projects/timeline/1967/01_images/truck-centenninal-mh-1967.png" : picture}
+                src={picture}
                 alt={"where the picture will be"}
-                className="rounded-md w-[90%] m-auto mt-4 mb-2"
+                className="rounded-[50%] w-32 m-auto mt-4 my-20 object-cover mb-2 h-32"
             />
+            </div>
             {/* <h2>{thisCardUserData.first_name}</h2> */}
-            <h4 className="ml-[5%] text-mg text-black font-bold w-fit">{thisCardUserData.first_name} <span className="p-0 m-0 w-fit text-sm text-gray-600">{'maybe where age will go'}</span></h4>
-            <p className="ml-[5%] text-sm text-gray-600 font-bold">Truck Brand: {truck_brand}</p>
-            <div className="flex [&>*]:mx-[8%] [&>*]:hover:cursor-pointer flex-row items-center w-full justify-center pb-2 mt-2">
+            <h4 className="ml-[10%] text-mg pt-5 text-black font-bold w-fit">{fullName}<span className="p-0 m-0 w-fit text-sm text-gray-600">{'age'}</span></h4>
+            <p className="ml-[10%] text-sm text-gray-600 font-bold">Truck Brand: {truck_brand}</p>
+            <div className="flex [&>*]:mx-[8%] [&>*]:hover:cursor-pointer flex-row items-center mx-auto py-5 w-full justify-center pb-5 mt-2">
                 <img onClick={() => handleLike()} className="h-10 w-10" src="https://cdn-icons-png.flaticon.com/512/1182/1182720.png"/>
                 <img onClick={() => handleDislike()} className="h-10 w-10" src="https://cdn-icons-png.flaticon.com/512/1533/1533919.png"/>
                 <img onClick={() => setIsClicked(!isClicked)} src="https://cdn-icons-png.flaticon.com/512/5791/5791540.png" className="h-10 w-10" alt="flip"/>
@@ -84,6 +88,7 @@ export default function RenderProfileCard({ picture, beard, moonshine, rodeo_buc
 
     const profileCardBack = (
         <>
+            <img className="rounded-md w-[90%] h-32 m-auto mt-4 mb-2" alt={"where the picture will be"} src="https://www.chevytrucklegends.com/content/dam/Projects/timeline/1967/01_images/truck-centenninal-mh-1967.png"/>
             <h2>{thisCardUserData.first_name} {thisCardUserData.last_name}</h2>
             <p>Beard Length 🧔‍♀️ : {beard}</p>
             <p>Preffered Tobaccy 🚬: {mode_of_tobacco}</p>
@@ -97,8 +102,8 @@ export default function RenderProfileCard({ picture, beard, moonshine, rodeo_buc
     )
 
     return(
-        <div className="w-1/4 h-[50%] ml-20 my-10 bg-white/70 backdrop-blur-sm rounded-md relative flex flex-col">
-            {isClicked ? profileCardBack : profileCardFront}
+        <div className="w-1/4 ml-20 my-10 bg-white/70 backdrop-blur-sm rounded-md relative flex flex-col">
+        {isClicked ? profileCardBack : profileCardFront}
             {/* <img id={style.car} className="h-10 w-10 absolute bottom-2 z-10" src="https://cdn-icons-png.flaticon.com/512/5440/5440927.png" alt="car"/>
             <img className="h-20 ml-4 -bottom-3 absolute z-0" src="https://cdn-icons-png.flaticon.com/512/358/358726.png" alt="road"/>
             <div className="h-20 ml-4 -bottom-3 absolute z-0">
